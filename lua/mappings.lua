86d60c611f5a1df6a25b16649fc4171b10d7b4c8
leosama <chigozieuduh.cu@gmail.com>
2024-10-09 16:35:04 +0100

    max line width ruler added

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,9 +8,3 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-
-
---Enable relative numbers
-vim.wo.relativenumber = true
-vim.o.tabstop = 4
-vim.o.expandtab = true
--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,11 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+--Enable relative numbers
+vim.wo.relativenumber = true
+vim.o.tabstop = 4
+vim.o.expandtab = true
+
+--Max line width ruler
+vim.o.colorcolumn="80"
