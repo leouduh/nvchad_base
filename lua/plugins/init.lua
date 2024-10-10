c16c5fe9dad3e7c3ae4c9e567dc911c2eb1521a7
leosama <chigozieuduh.cu@gmail.com>
2024-10-10 12:28:38 +0100

    added nvim surround plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,4 +23,15 @@ return {
   	},
   },
 
+  {
+    "kylechui/nvim-surround",
+    version="*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+
+      })
+    end
+  },
+
 }
