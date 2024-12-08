2d7dae9afbd9e298331b2070237b01e4210d93fd
Chigozie Uduh <chigozieuduh@MAC-0A7258.station>
2024-12-08 15:01:41 +0000

    Adding lua lsp to lsp_config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" , "gopls", "pyright", "bashls"}
+local servers = { "html", "cssls" , "gopls", "pyright", "bashls", "clangd", "lua_ls"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
